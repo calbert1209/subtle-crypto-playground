ea4ae8cacb77c0ce13e2f2b4128153bb3d06f5f5
Christian Albert <calbert1209@gmail.com>
2024-04-28 00:00:04 +0900

    decrypt from vault values

--- example/vault-example.js
+++ example/vault-example.js
@@ -4,23 +4,12 @@ import { AesKw } from "./algorithms/aes-kw.js";
 import { Aes } from "./algorithms/aes.js";
 import { ALGO_NAMES, KEY_USAGE } from "./utilities/constants.js";
 
-/*
-Wrap the given key.
-*/
-async function wrapCryptoKey(aesKw, keyToWrap) {
-  // get the key encryption key
-  const password = window.prompt("Enter your password");
-  return aesKw.wrapKey(keyToWrap, password);
-}
-
-/*
-Generate an encrypt/decrypt secret key,
-then wrap it.
-*/
 (async function main() {
   const secret = window.prompt("Enter your secret");
   const password = window.prompt("Enter your password");
   const tc = new TextCoding();
+
+  // create key KE to encrypt/decrypt secret
   const keyToWrap = await window.crypto.subtle.generateKey(
     {
       name: ALGO_NAMES.AES_GCM,
@@ -29,6 +18,9 @@ then wrap it.
     true,
     [KEY_USAGE.encrypt, KEY_USAGE.decrypt]
   );
+  await Utils.logAESKey("keyToWrap", keyToWrap);
+
+  // encrypt secret via KE
   const iv = window.crypto.getRandomValues(new Uint8Array(12));
   const cipherText = await window.crypto.subtle.encrypt(
     { name: ALGO_NAMES.AES_GCM, iv },
@@ -36,15 +28,19 @@ then wrap it.
     tc.toArrayBuffer(secret)
   );
 
-  await Utils.logAESKey("keyToWrap", keyToWrap);
+  // wrap KE via AES-KW
   const aesKw = new AesKw(tc);
   const { wrappedKey, wrappingKey } = await aesKw.wrapKey(keyToWrap, password);
   console.log(
     "%c ::: wrapped key ::: \n",
     "background-color: blue;color:white;",
-    Utils.toHexString(wrappedKey)
+    Utils.toHexString(wrappedKey),
+    new Uint8Array(wrappedKey)
   );
 
+  await Utils.logAESKey("wrappingKey", wrappingKey);
+
+  // create Entry
   const cipherObject = {
     cipherText: Utils.toHexString(cipherText),
     iv: Utils.toHexString(iv),
@@ -57,13 +53,13 @@ then wrap it.
     cipherObject
   );
 
-  // create vault key from password
+  // create vault key KV from password
   const aes = new Aes(tc);
   const { salt: vKeySalt, key: vKey } = await aes.deriveKeyFromPassword(
     password
   );
 
-  // encrypt vault
+  // encrypt vault using KV
   const cipherJson = JSON.stringify({ entries: [cipherObject] });
   const vIv = window.crypto.getRandomValues(new Uint8Array(12));
   const vCipherText = await window.crypto.subtle.encrypt(
@@ -83,14 +79,37 @@ then wrap it.
     vCipherObject
   );
 
-  await Utils.logAESKey("wrappingKey", wrappingKey);
-  const unwrapKey = await aesKw.unwrapKey(wrappedKey, wrappingKey);
+  // get
+  const { key: vaultDecryptKey } = await aes.deriveKeyFromPassword(
+    password,
+    Utils.fromHex(vCipherObject.salt)
+  );
+
+  const buffer = await window.crypto.subtle.decrypt(
+    {
+      name: ALGO_NAMES.AES_GCM,
+      iv: vIv,
+    },
+    vaultDecryptKey,
+    Utils.fromHex(vCipherObject.ct)
+  );
+
+  const dVault = JSON.parse(tc.fromArrayBuffer(buffer));
+  const [dEntry] = dVault.entries;
+  const dWrappedKey = Utils.fromHex(dEntry.key);
+  const dCt = Utils.fromHex(dEntry.cipherText);
+  const dIv = Utils.fromHex(dEntry.iv);
+
+  console.log("decrypt vault key", dWrappedKey);
+
+  // const unwrapKey = await aesKw.unwrapKey(wrappedKey, wrappingKey);
+  const unwrapKey = await aesKw.unwrapKey(dWrappedKey, wrappingKey);
   await Utils.logAESKey("unwrappedKey", unwrapKey);
 
   const decrypted = await window.crypto.subtle.decrypt(
-    { name: ALGO_NAMES.AES_GCM, iv },
+    { name: ALGO_NAMES.AES_GCM, iv: dIv },
     unwrapKey,
-    cipherText
+    dCt
   );
   console.log(
     "%c ::: decrypted text ::: \n",
